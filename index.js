882f8147686469e3cdb987b1f57c4fa384892cb0
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-06-29 22:35:20 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+// Function to non-destructively update an employee with a key and value
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  // Function to destructively update an employee with a key and value
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Function to non-destructively delete a key from an employee
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Function to destructively delete a key from an employee
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  // Sample employee object for testing
+  let employee = {
+    name: 'Sam'
+  };
+  
